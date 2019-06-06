6eed92234b42cfee73d82ddb743b2ccfb64bd631
boruch d <dubywider@gmail.com>
2019-06-06 22:13:13 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,11 +1,14 @@
 class Patient
 
    attr_reader :name, :appointment, :doctor
-
+@@all = []
    def initialize(name)
     @name = name
+    @@all << self
   end
-
+def self.all
+  @@all
+end 
    def new_appointment(doctor, date)
     Appointment.new(self, doctor, date)
   end
