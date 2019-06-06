3956358ee40031a0a89c6246a9236486c8f0ded9
boruch d <dubywider@gmail.com>
2019-06-06 21:38:56 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,16 @@
+class Artist
+   attr_accessor :name, :artist, :genre
+@@all = []
+  def initialize (name)
+    @name = name 
+    @@all << self
+  end 
+def self.all 
+  @@all 
+  
+end 
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end
+end 
---
+++ lib/genre.rb
@@ -0,0 +1,9 @@
+class Genre 
+  attr_reader :name, :genre
+  attr_accessor :song 
+
+  def initialize (name)
+    @name = name
+    
+  end 
+end 
